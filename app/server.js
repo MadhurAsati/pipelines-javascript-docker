7f5315b6abb93982a0c4a9522aa83fe1f6bb28d5
MadhurAsati <madhur9827@gmail.com>
2020-01-09 13:06:22 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world and hello Madhur\n');
+  res.send('Hello world and hello Madhur and Hello India\n');
 });
 
 app.listen(PORT, HOST);
